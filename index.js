24903fbeb214364cd3d6f6e5a9cff7f8a2f46de2
Jeremy Lu <example@example.com>
2015-06-28 11:33:34 +0800

    t

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
 var http = require('http');
+
 http.createServer(function (req, res) {
+	console.log( '請求來了' );
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(8080);
+
+console.log('Server started');
