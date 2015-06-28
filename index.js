be39fa2dccac8c4f55d72c4e6e85820f1668294e
Jeremy Lu <example@example.com>
2015-06-28 11:35:02 +0800

    t

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	console.log( '請求來了' );
+	console.log( '請求來了: ', new Date().toString() );
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080);
